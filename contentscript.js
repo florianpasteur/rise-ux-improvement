a36bdbd8dba5d4a1447105e6508e9b88ac90d361
Florian <florian@backbase.com>
2021-12-06 12:22:23 +0100

    Make course folder bookmarkable

--- contentscript.js
+++ contentscript.js
@@ -2,14 +2,41 @@
 
 const nodeUpdated = [];
 
+function updateNodes(selector, fn) {
+    Array.from(document.querySelectorAll(selector))
+        .filter(e => !nodeUpdated.includes(e))
+        .forEach((element, index, array) => {
+        fn(element, index, array)
+        nodeUpdated.push(element);
+    })
+}
+
 setInterval(() => {
-    Array.from(document.querySelectorAll('div[data-ba-course-id]')).filter(e => !nodeUpdated.includes(e)).forEach(e => {
-        var a = document.createElement('a');
-        a.href = window.location + 'author/' + e.attributes['data-ba-course-id'].value;
+
+    updateNodes('div[data-ba-course-id]', e => {
+        const a = document.createElement('a');
+        a.href = 'https://rise.articulate.com/author/' + e.attributes['data-ba-course-id'].value;
         a.target = "_blank";
         a.innerText = "Open in new tab";
         a.classList.add("custom-button-1");
         e.appendChild(a);
-        nodeUpdated.push(e);
     })
+
+    updateNodes('.course-folder > button',(e, i) => {
+        e.addEventListener('click', function () {
+            window.location = ("" + window.location).replace(/#[A-Za-z0-9_\-]*$/, '') + "#folder-" + i;
+        })
+    });
 }, 1000);
+
+
+let anchorJumpInterval = setInterval(() => {
+    let folderButtons = Array.from(document.querySelectorAll('.course-folder > button'));
+    if (folderButtons.length > 3) {
+        const folderIndex = parseInt(window.location.hash.replace("#folder-", ''));
+        if (!isNaN(folderIndex) && folderIndex > 0 && folderButtons[folderIndex]) {
+            folderButtons[folderIndex].click();
+            clearInterval(anchorJumpInterval);
+        }
+    }
+}, 500);
