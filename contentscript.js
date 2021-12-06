1d68dd7b93c3569b15d421e304023bd016726a05
Florian <florian@backbase.com>
2021-12-06 11:27:41 +0100

    Add style

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,15 @@
-document.querySelectorAll('div[data-ba-course-id]').forEach(e => {
-    var a = document.createElement('a');
-    a.href = window.location + '/author/' +e.attributes['data-ba-course-id'].value;
-    a.target = "_blank";
-    a.innerText="New Tab";
-    e.appendChild(a);
-})
+
+
+const nodeUpdated = [];
+
+setInterval(() => {
+    Array.from(document.querySelectorAll('div[data-ba-course-id]')).filter(e => !nodeUpdated.includes(e)).forEach(e => {
+        var a = document.createElement('a');
+        a.href = window.location + 'author/' + e.attributes['data-ba-course-id'].value;
+        a.target = "_blank";
+        a.innerText = "Open in new tab";
+        a.classList.add("custom-button-1");
+        e.appendChild(a);
+        nodeUpdated.push(e);
+    })
+}, 1000);
