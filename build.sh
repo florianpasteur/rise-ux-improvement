de9411e1bea5a71f8295dd19dba4e5e63693c3f6
Florian PASTEUR <florian.c.p@pm.me>
2022-01-26 14:41:30 +0100

    Change build method

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
-zip dist/rise-ux-improvement-$COMMIT_HASH.zip icons contentscript.js manifest.json mvp.css style.css popup.html popup.js
+#zip dist/rise-ux-improvement-$COMMIT_HASH.zip icons contentscript.js manifest.json mvp.css style.css popup.html popup.js
+
+rm -fr dist/.tmp
+mkdir -p dist/.tmp
+cp icons contentscript.js manifest.json mvp.css style.css popup.html popup.js dist/.tmp
+crx3 dist/.tmp --crx dist/rise-ux-improvement-$COMMIT_HASH.crx --appVersion 1.0.2
