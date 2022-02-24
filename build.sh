2f253f63f53f4fc670e338ae42a90fb2ded8daa2
Florian PASTEUR <florian.c.p@pm.me>
2022-02-24 13:57:52 +0100

    Clean up code

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-COMMIT_HASH=$(git rev-parse --short HEAD)
-#zip dist/rise-ux-improvement-$COMMIT_HASH.zip icons contentscript.js manifest.json mvp.css style.css popup.html popup.js
-
-rm -fr dist/.tmp
-mkdir -p dist/.tmp
-cp -R icons contentscript.js manifest.json mvp.css style.css popup.html popup.js dist/.tmp
-crx3 dist/.tmp --crx dist/rise-ux-improvement-$COMMIT_HASH.crx --appVersion 1.0.2
